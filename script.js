d5492d6669af22536d6f247336a36f413edc00aa
joop <joop@joop.com>
2024-09-08 20:10:48 +0200

    normal size

--- script.js
+++ script.js
@@ -1,109 +1,5 @@
 //kaart wordt 50 x 50 pt
 
-world = Array()
-
-class Tile { // Tegel
-    constructor(arr) { // array [0,1,2,0] is NW niets, NE muur, SE deur, SW niets
-        this.arr = arr;
-        this.rotation = 0;
-    }
-
-    drawSegment(ctx, x, y, px, py) {
-        ctx.beginPath();
-        ctx.moveTo(x * 50 + 25, y * 50 + 25);
-        ctx.lineTo(x * 50 + 25 + px, y * 50 + 25 + py);
-        ctx.stroke();
-    }
-
-    getSegment(i) {
-        return this.arr[(i + this.rotation) % 4];
-    }
-
-    draw(ctx, x, y) {
-        let rotated = rotateLeft(this.arr, this.rotation);
-
-        if (rotated[0] >= 1) {
-            this.drawSegment(ctx, x, y, -25, -25);
-        }
-        if (rotated[0] == 2) {
-            ctx.beginPath();
-            ctx.arc(x * 50 + 10, y * 50 + 10, 10, 0, 2 * Math.PI);
-            ctx.stroke();
-        }
-        if (rotated[1] >= 1) {
-            this.drawSegment(ctx, x, y, 25, -25);
-        }
-        if (rotated[1] == 2) {
-            ctx.beginPath();
-            ctx.arc(x * 50 + 40, y * 50 + 10, 10, 0, 2 * Math.PI);
-            ctx.stroke();
-        }
-        if (rotated[2] >= 1) {
-            this.drawSegment(ctx, x, y, 25, 25);
-        }
-        if (rotated[2] == 2) {
-            ctx.beginPath();
-            ctx.arc(x * 50 + 40, y * 50 + 40, 10, 0, 2 * Math.PI);
-            ctx.stroke();
-        }
-        if (rotated[3] >= 1) {
-            this.drawSegment(ctx, x, y, -25, 25);
-        }
-        if (rotated[3] == 2) {
-            ctx.beginPath();
-            ctx.arc(x * 50 + 10, y * 50 + 40, 10, 0, 2 * Math.PI);
-            ctx.stroke();
-        }
-    }
-}
-
-var mult = 300;
-var tiles = [];
-for (let i = 0; i < 15 * mult; ++i) {
-    tiles.push(new Tile([1,0,1,0]));
-}
-for (let i = 0; i < 7 * mult; ++i) {
-    tiles.push(new Tile([0,0,1,1]));
-}
-for (let i = 0; i < 5 * mult; ++i) {
-    tiles.push(new Tile([0,0,0,0]));
-}
-for (let i = 0; i < 2 * mult; ++i) {
-    tiles.push(new Tile([1,0,2,0]));
-}
-for (let i = 0; i < 2 * mult; ++i) {
-    tiles.push(new Tile([0,0,1,2]));
-}
-for (let i = 0; i < 2 * mult; ++i) {
-    tiles.push(new Tile([0,0,2,1]));
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        // Generate a random index from 0 to i
-        let j = getRandomInt(i + 1);
-        
-        // Swap elements array[i] and array[j]
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
-
-function rotateLeft(array, positions) {
-  return array.slice(positions).concat(array.slice(0, positions));
-}
-
-function rotateLeftInPlace(array, positions) {
-    let length = array.length;
-    positions = positions % length; // Handle cases where positions > length
-    let removed = array.splice(0, positions);
-    array.push(...removed);
-}
-
 class World {
     constructor(width, height) {
         this.width = width;
@@ -217,12 +113,113 @@ class World {
     }
 }
 
-var world = new World(100, 100);
-world.create();
-
-//document.getElementById("demo").innerHTML = "Hello JavaScript!";
-// Create a Canvas:
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+
+var world = new World(canvas.width / 50, canvas.height / 50);
+
+class Tile { // Tegel
+    constructor(arr) { // array [0,1,2,0] is NW niets, NE muur, SE deur, SW niets
+        this.arr = arr;
+        this.rotation = 0;
+    }
+
+    drawSegment(ctx, x, y, px, py) {
+        ctx.beginPath();
+        ctx.moveTo(x * 50 + 25, y * 50 + 25);
+        ctx.lineTo(x * 50 + 25 + px, y * 50 + 25 + py);
+        ctx.stroke();
+    }
+
+    getSegment(i) {
+        return this.arr[(i + this.rotation) % 4];
+    }
+
+    draw(ctx, x, y) {
+        let rotated = rotateLeft(this.arr, this.rotation);
+
+        if (rotated[0] >= 1) {
+            this.drawSegment(ctx, x, y, -25, -25);
+        }
+        if (rotated[0] == 2) {
+            ctx.beginPath();
+            ctx.arc(x * 50 + 10, y * 50 + 10, 10, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+        if (rotated[1] >= 1) {
+            this.drawSegment(ctx, x, y, 25, -25);
+        }
+        if (rotated[1] == 2) {
+            ctx.beginPath();
+            ctx.arc(x * 50 + 40, y * 50 + 10, 10, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+        if (rotated[2] >= 1) {
+            this.drawSegment(ctx, x, y, 25, 25);
+        }
+        if (rotated[2] == 2) {
+            ctx.beginPath();
+            ctx.arc(x * 50 + 40, y * 50 + 40, 10, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+        if (rotated[3] >= 1) {
+            this.drawSegment(ctx, x, y, -25, 25);
+        }
+        if (rotated[3] == 2) {
+            ctx.beginPath();
+            ctx.arc(x * 50 + 10, y * 50 + 40, 10, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+    }
+}
+
+var mult = (world.width * world.height) / 33;
+var tiles = [];
+for (let i = 0; i < 15 * mult; ++i) {
+    tiles.push(new Tile([1,0,1,0]));
+}
+for (let i = 0; i < 7 * mult; ++i) {
+    tiles.push(new Tile([0,0,1,1]));
+}
+for (let i = 0; i < 5 * mult; ++i) {
+    tiles.push(new Tile([0,0,0,0]));
+}
+for (let i = 0; i < 2 * mult; ++i) {
+    tiles.push(new Tile([1,0,2,0]));
+}
+for (let i = 0; i < 2 * mult; ++i) {
+    tiles.push(new Tile([0,0,1,2]));
+}
+for (let i = 0; i < 2 * mult; ++i) {
+    tiles.push(new Tile([0,0,2,1]));
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        // Generate a random index from 0 to i
+        let j = getRandomInt(i + 1);
+        
+        // Swap elements array[i] and array[j]
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function rotateLeft(array, positions) {
+  return array.slice(positions).concat(array.slice(0, positions));
+}
+
+function rotateLeftInPlace(array, positions) {
+    let length = array.length;
+    positions = positions % length; // Handle cases where positions > length
+    let removed = array.splice(0, positions);
+    array.push(...removed);
+}
+
+world.create();
 
 world.draw(ctx);
